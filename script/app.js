c5a2f3485df3cf16e2a635bd57129958675dd374
D3RP <hudson.curren@icloud.com>
2020-03-26 17:20:12 +1300

    added more images fixed styling optimised js

--- script/app.js
+++ script/app.js
@@ -4,53 +4,70 @@ const navButton = document.querySelector(".nav-button");
 const navWidth = "20vw";
 const main = document.querySelector("main");
 const navButtons = document.querySelectorAll(".nav-item");
+var navOpen = false;
 
-// IMPORTING BARBA AND INITIALIZING
-barba.use(barbaCss);
-barba.use(barbaPrefetch)
-barba.init();
-
-// INITALIZING ANIMATE ON-SCROLL
-AOS.init({
-    disable: ['phone', 'tablet', 'mobile'],
-    startEvent: 'DOMContentLoaded',
-    useClassNames: true,
-
-    // DEFAULT SETTING
-    duration: 250,
-    easing: 'ease-out-quint',
-    mirror: false
-});
-
-// REFRESHING AOS ANIMATIONS WHEN WINDOW IS RESIZED
-window.addEventListener('resize', function () {
-    AOS.refresh();
-})
-
-// closing sidenav when a button is pressed
-navButtons.forEach(item => {
-    item.addEventListener('click', event => {
-        toggleNav();
-        console.log(event);
+function makeNavLookNice() {
+    navButton.addEventListener('mouseenter', () => {
+        if (!navOpen) {
+            nav.classList.add("shadow");
+        }
     });
-})
 
-// SETTING UP TOGGLENAV FUNCTION, CALLED BY EVENT LISTENER
+    navButton.addEventListener('mouseleave', () => {
+        if (!navOpen) {
+            nav.classList.remove("shadow");
+        }
+    });
+
+    document.querySelector('main').addEventListener('click', () => {
+        if (navOpen) {
+            toggleNav();
+            nav.classList.remove("shadow");
+        }
+    });
+
+    navButton.addEventListener("click", function () {
+        toggleNav();
+    });
+}
+
+function prepNavButtons() {
+    navButtons.forEach(item => {
+        item.addEventListener('click', event => {
+            toggleNav();
+            console.log(event);
+        });
+    });
+}
+
 function toggleNav() {
     if (nav.style.width == "0px") {
         nav.style.width = navWidth;
-        nav.classList.add("shadow");
-        navButton.innerHTML = "close"
+        navButton.innerHTML = "close";
+        navOpen = true;
 
     } else {
         nav.style.width = "0px";
-        nav.classList.remove("shadow");
-        navButton.innerHTML = "menu"
+        navButton.innerHTML = "menu";
+        navOpen = false;
     }
 }
 
-navButton.addEventListener("click", function () {
-    toggleNav();
-});
+function resetNav() {
+    nav.style.width = "0px";
+    nav.classList.remove("shadow");
+    navOpen = false;
+}
 
-document.body.classList.remove('loading');
+function init() {
+    prepNavButtons();
+    makeNavLookNice();
+    resetNav();
+    AOS.init({
+        'easing': 'ease-out-quint',
+        'duration': '300ms'
+    });
+    document.body.classList.remove("loading");
+}
+
+init();
