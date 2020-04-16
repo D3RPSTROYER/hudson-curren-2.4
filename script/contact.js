4f91cc18f216c2daa2b2c6bcfbcc8b50fa1b04f6
D3RP <hudson.curren@icloud.com>
2020-04-16 16:09:15 +1200

    modified the script to make it easier to actually send a request down the line

--- script/contact.js
+++ script/contact.js
@@ -2,19 +2,20 @@ var textarea = document.querySelector('textarea');
 var nameField = document.querySelector('.name-field');
 var emailField = document.querySelector('.email-field');
 var submitButton = document.querySelector('.submit-button');
+var Message = {
+    name: '',
+    email: '',
+    message: ''
+}
 
 var message, name, email;
 
 function submit() {
-    if (nameField.value && emailField.value && nameField.value) {
-        message = textarea.value;
-        console.log(message);
-        name = nameField.value;
-        console.log(name);
-        email = emailField.value;
-        console.log(email);
-    }
-    else {
-        alert('Sorry, you forgot one or more boxes, please ensure that you\'ve filled out the Name, Email and Message boxes');
-    }
+    message = textarea.value;
+    name = nameField.value;
+    email = emailField.value;
+    Message.name = name;
+    Message.email = email;
+    Message.message = message;
+    console.log(Message);
 }
